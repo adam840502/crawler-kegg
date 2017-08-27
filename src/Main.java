876940a82cb3e7f2c62033cd5a17007bc7cb4dd9
adam840502 <adam840502@github.com>
2017-08-27 16:19:51 +0800

    finish search engine

--- src/Main.java
+++ src/Main.java
@@ -14,35 +14,65 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		makeOrganismList();
+		System.out.println("Searching for organism list...");
+		OrganismList ol = makeOrganismList();
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.print("enter key words: ");
-		String keyWord = sc.nextLine();
+		String keyword = sc.nextLine();
 		
+		search(ol, keyword);
 		
 		
 	}
 	
-	static boolean makeOrganismList(){
-		
+	/**
+	 * title: makeOrganismList
+	 * param: 
+	 * return: OrganismList object of the list
+	 */
+	static OrganismList makeOrganismList(){
+		OrganismList ol = new OrganismList();
 		try {
 			Connection con = Jsoup.connect("http://rest.kegg.jp/list/organism");
-			
 			String s = con.execute().body();
-			OrganismList ol = new OrganismList(s);
-		
-			
+			ol = new OrganismList(s);
 		} catch (IOException e) {
-			return false;
+			e.printStackTrace();
 		}
-		
-		return true;
+		return ol;
+	}
+	
+	static boolean search(OrganismList ol, String keyword){
+		boolean ret = false;
+		List<Organ> match = ol.search(keyword);
+		if(match.isEmpty()){
+			System.out.println("no matches.");
+		}else{
+			System.out.println("matches("+match.size()+"):");
+			int i =1;
+			for(Organ or : match){
+				System.out.print(i+".\t");
+				System.out.print(or.TNumber);
+				System.out.print("  ");
+				System.out.print(or.org);
+				System.out.print("  ");
+				System.out.print(or.scientificName);
+				System.out.print("    ");
+				System.out.print(or.keywords);
+				System.out.println();
+				++i;
+			}
+			ret = true;
+		}
+		return ret;
 	}
 }
 
 class OrganismList {
 	List<Organ> list = new ArrayList<Organ>();
+	
+	OrganismList(){}
 	
 	OrganismList(String data){
 		
@@ -82,9 +112,26 @@ class OrganismList {
 				e.printStackTrace();
 			}
 			Organ or = new Organ(TNumber, org, NameRaw, keyWordsRaw);
-			System.out.println(or.scientificName+"\t"+or.commomName);
+			//System.out.println(or.TNumber+"  "+or.org+"  "+or.scientificName+", "+
+			//					or.commomName+", "+or.keyWords);
 			list.add(or);
 		}
+	}
+
+	List<Organ> search(String keyword){
+		List<Organ> match = new ArrayList<>();
+		for(Organ or : list){
+			boolean tNumMatch = or.TNumber.matches("(.|\n)*"+keyword+"(.|\n)*");
+			boolean sciMatch = or.scientificName.matches("(.|\n)*"+keyword+"(.|\n)*");
+			boolean keyMatch = false;
+			for(String s : or.keywords){
+				if(s.matches("(.|\n)*"+keyword+"(.|\n)*")) keyMatch = true;
+			}
+			if(tNumMatch | sciMatch | keyMatch){
+				match.add(or);
+			}
+		}
+		return match;
 	}
 }
 
@@ -92,11 +139,10 @@ class Organ{
 	String TNumber;
 	String org;
 	String scientificName;
-	String commomName;
-	List<String> keyWords;
+	List<String> keywords;
 	
 	Organ(String TNumber, String org, String NameRaw, String keyWordsRaw){
-		keyWords = new ArrayList<>();
+		keywords = new ArrayList<>();
 		
 		this.TNumber = TNumber;
 		this.org = org;
@@ -105,23 +151,33 @@ class Organ{
 		String word = "";
 		
 		InputStream nameIs = new ByteArrayInputStream(NameRaw.getBytes());
-		boolean hasCommomName = false;
+		boolean hasParens = false;
+		int parensNum = 0;
 		try {
 			while((cell=nameIs.read())>0){
 				if(cell == 32){
 					if((cell=nameIs.read()) == 40){
-						hasCommomName = true;
-						this.scientificName = word;
+						hasParens = true;
+						++parensNum;
+						if(parensNum==1){
+							this.scientificName = word;
+						}else{
+							keywords.add(word);
+						}
 						word = "";
 					}else{
 						word += " ";
 						word += Character.toString((char)cell);
 					}
 				}else if(cell == 41){
-					this.commomName = word;
+					this.keywords.add(word);
+					word = "";
 				}else{
 					word += Character.toString((char)cell);
 				}
+			}
+			if(!hasParens){
+				this.scientificName = word;
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -133,7 +189,7 @@ class Organ{
 		try {
 			while((cell=keyIs.read())>0){
 				if(cell == 59){
-					keyWords.add(word);
+					keywords.add(word);
 					word = "";
 				}else{
 					word += Character.toString((char)cell);
@@ -143,6 +199,6 @@ class Organ{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		keyWords.add(word);
+		keywords.add(word);
 	}
 }
